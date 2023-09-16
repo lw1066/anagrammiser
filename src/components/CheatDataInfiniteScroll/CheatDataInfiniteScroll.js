08a68355cb0e4148b33eaae3cd44fd11501f4fc8
lw1066 <100121171+lw1066@users.noreply.github.com>
2023-09-16 18:41:47 +0000

    removed infinite scrolling as causing mobile issues and not neeeded

--- src/components/CheatDataInfiniteScroll/CheatDataInfiniteScroll.js
+++ src/components/CheatDataInfiniteScroll/CheatDataInfiniteScroll.js
@@ -1,68 +1,53 @@
-import React, { useState, useEffect, useRef, useCallback } from "react";
+import React, { useState, useEffect } from "react";
 import Button from '../../UI/Button/Button';
 import { CheatScrollerItem } from '../CheatScrollerItem/CheatScrollerItem';
 import classes from './CheatDataInfiniteScroll.module.css';
 
-
-
 export const CheatDataInfiniteScroll = (props) => {
     const [visibleItems, setVisibleItems] = useState([]);
     const [offset, setOffset] = useState(0);
-    const containerRef = useRef(null);
-    const anagramNumber = props.cheatData.length
-    
-    const loadMoreItems = useCallback(() => {
-        const batchSize = 20; // You can adjust the batch size
+    const batchSize = 20; // Set your batch size here
+
+    // Function to load items initially and when "Load More" is clicked
+    const loadItems = () => {
         const newVisibleItems = props.cheatData.slice(0, offset + batchSize);
         setVisibleItems(newVisibleItems);
         setOffset(offset + batchSize);
-    }, [props.cheatData, offset]);
-    
-    useEffect(() => {
-        // Initialize Intersection Observer
-        const options = {
-        root: null, // Use the viewport as the root
-        rootMargin: "0px",
-        threshold: 0.1, // When 10% of the container is visible, trigger the callback
-        };
-    
-        const container = containerRef.current;
+    };
 
-        const observer = new IntersectionObserver(([entry]) => {
-        if (entry.isIntersecting) {
-            loadMoreItems();
-        }
-        }, options);
-    
-        if (container) {
-        observer.observe(container);
-        }
-    
-        // Cleanup the observer
-        return () => {
-        if (container) {
-            observer.unobserve(container);
-        }
-        };
-    }, [props.cheatData, offset, loadMoreItems]);
+    // Load items initially when the component mounts
+    useEffect(() => {
+        loadItems();
+    }, []);
+
+    // Function to load more items when "Load More" is clicked
+    const loadMoreItems = () => {
+        loadItems();
+    };
 
     return (
         <div>
             <div className={classes.backdrop} onClick={props.onConfirm}/>
-                <div className={classes.modal}>
-                    <header className={classes.header}>
-                        <h2>{anagramNumber === 0 ? 'No' : anagramNumber} anagram{anagramNumber > 1 ? 's': ''} found of these letters: {props.letters} </h2>
-                    </header>
-                    <div className={classes.content}>
-                        <div>
-                            <div ref={containerRef} style={{ height: "40vh", overflowY: "scroll" }}>
-                                {visibleItems.map((item, index) => (
+            <div className={classes.modal}>
+                <header className={classes.header}>
+                    <h2>{props.cheatData.length === 0 ? 'No' : props.cheatData.length} anagram{props.cheatData.length > 1 ? 's': ''} found of these letters: {props.letters} </h2>
+                </header>
+                <div className={classes.content}>
+                    <div>
+                        <div style={{ height: "40vh", overflowY: "scroll" }}>
+                            {visibleItems.map((item, index) => (
                                 <div key={index}><CheatScrollerItem word={item.word} defs={item.defs} /></div>
-                                ))}
-                            </div>
-                        </div>  
-                    </div>
+                            ))}
+                        </div>
+                        
+                    </div>  
+                </div>
                 <footer className={classes.actions}>
+                    {offset < props.cheatData.length && (
+                        <div className={classes.loadMoreButton}>
+                            <Button onClick={loadMoreItems}>Load More</Button>
+                        </div>
+                    )}
                     <Button onClick={props.onConfirm}>Okay</Button>
                 </footer>
             </div>
