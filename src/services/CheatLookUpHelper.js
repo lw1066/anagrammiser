8dc1b3b36c52ae992f9c3a6cdf0065e553334ae7
lw1066 <100121171+lw1066@users.noreply.github.com>
2023-09-16 18:55:05 +0000

    fixed state dependency

--- src/services/CheatLookUpHelper.js
+++ src/services/CheatLookUpHelper.js
@@ -1,42 +1,39 @@
- import { CustomError } from '../UI/ErrorModal/CustomError';
+import { CustomError } from '../UI/ErrorModal/CustomError';
 
-    // DATAMUSE LOOKUP 
-    export const CheatLookUpHelper = async (cheatWord, letters, errorHandler) => {
-        const pattern = cheatWord.map((letter) => (letter === '' ? '.' : letter));
-        const regexPattern = pattern.join('');
-        const regex = new RegExp(`^${regexPattern}$`, 'i');
-        const datamusePrep = '//' + letters.join('') + '//';
+export const CheatLookUpHelper = async (cheatWord, letters, errorHandler) => {
+  const pattern = cheatWord.map((letter) => (letter === '' ? '.' : letter));
+  const regexPattern = pattern.join('');
+  const regex = new RegExp(`^${regexPattern}$`, 'i');
+  const datamusePrep = '//' + letters.join('') + '//';
 
-        console.log(`datamusePrep: ${datamusePrep}`)
+  try {
+    const response = await fetch(`https://api.datamuse.com/words?sp=${datamusePrep}&max=100&md=d`);
+    if (!response.ok) {
+      throw new CustomError('Error', `There is a problem at datamuse (${response.status})`);
+    }
 
-        try {
-          const response = await fetch(`https://api.datamuse.com/words?sp=${datamusePrep}&max=100&md=d`);
-          if (!response.ok) {
-            throw new CustomError('Error', `There is a problem at datamuse (${response.status})`);
-          }
-      
-          const data = await response.json();
-          console.log("API Response:", data);
-;
+    const data = await response.json();
+    
+    if (data.length === 0){
+      throw new CustomError('Nothing found', `There aren't any results on datamuse for that collection of letters`);
+    }
 
-          if (data.length === 0){
-            throw new CustomError('Nothing found', `There aren't any results on datamuse for that collection of letters`);
-          }
-      
-          const filteredData = data.filter(item => {
-            const matchCondition = regex.test(item.word) && item.defs;
-            return matchCondition;
-          });
-      
-          if (filteredData.length === 0) {
-            throw new CustomError('Nothing with the letters added', `There are no anagram matches for this lot of letters - soz ${filteredData}`)
-          }
-          const cheatData = filteredData.map(item => ({
-            word: item.word,
-            defs: item.defs
-          }));
-          return cheatData;
-        } catch (error) {
-          errorHandler(error.name, error.message)
-        }
-      }
+    const filteredData = data.filter(item => {
+      const matchCondition = regex.test(item.word) && item.defs;
+      return matchCondition;
+    });
+
+    if (filteredData.length === 0) {
+      throw new CustomError('Nothing with the letters added', `There are no anagram matches for this lot of letters - soz ${filteredData}`)
+    }
+    
+    const cheatData = filteredData.map(item => ({
+        word: item.word,
+        defs: item.defs
+      })
+    );
+    return cheatData;
+  } catch (error) {
+    errorHandler(error.name, error.message)
+  }
+}
